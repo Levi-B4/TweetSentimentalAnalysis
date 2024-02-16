0b4b1389b56c06e017345723242c08a2b52fb6d2
L-Arch <levi44b@gmail.com>
2024-02-16 13:47:36 -0500

    cast char* params to DSString

--- sentementalanalysis.cpp
+++ sentementalanalysis.cpp
@@ -7,18 +7,16 @@
 #include <fstream>
 
 SentementalAnalysis::SentementalAnalysis(char* trainData, char* trainTarget, char* testData, char* testTarget, char* outputFile) {
-    files[0] = trainData;
-    files[1] = trainTarget;
+    files[0] = DSString(trainData);
+    files[1] = DSString(trainTarget);
 
-    files[2] = testData;
-    files[3] = testTarget;
+    files[2] = DSString(testData);
+    files[3] = DSString(testTarget);
 
-    files[4] = outputFile;
+    files[4] = DSString(outputFile);
 
 
     loadDataFile(files[0]);
-
-
 }
 
 void SentementalAnalysis::train(){
